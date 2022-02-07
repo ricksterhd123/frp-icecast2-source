aaada864468671e638926d151243ce43228f59ce
ricksterhd123 <ricksterhd123@gmail.com>
2022-02-07 01:33:37 +0000

    Added submodule redis++ and removed sqlite. Doing some research on threads atm

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include <string>
 #include <iostream>
 #include <thread>
-#include <streamer.h>
 #include <sw/redis++/redis++.h>
+#include "streamer.h"
 
 using namespace sw::redis;
 using namespace std;
 
 // Name of channel that we subscribe to
-const char* channel_name = "channel1";
+const char *channel_name = "channel1";
 // URI of redis server
-const char* redis_uri = "tcp://127.0.0.1:6379";
+const char *redis_uri = "tcp://127.0.0.1:6379";
 
 int main(int argc, char **argv)
 {
@@ -25,51 +25,52 @@ int main(int argc, char **argv)
 
     try
     {
-        // /**
-        //  * @brief Subscribe to channel in redis that provides us with commands
-        //  */
-
-        // cout << "Connecting to redis" << endl;
-        // auto redis = Redis(redis_uri);
-
-        // // Create a Subscriber.
-        // auto sub = redis.subscriber();
-
-        // // Set callback functions.
-        // sub.on_message(
-        //     [](std::string channel, std::string msg){
-        //         cout << channel << " : " << msg << endl;
-        //     }
-        // );
-
-        // // Subscribe to channels and patterns.
-        // sub.subscribe(channel_name);
-
-        // // Consume messages in a loop.
-        // while (true)
-        // {
-        //     try
-        //     {
-        //         sub.consume();
-        //     }
-        //     catch (const Error &err)
-        //     {
-        //         // Handle exceptions.
-        //         cout << err.what() << endl;
-        //     }
-        // }
-
         Streamer streamer("127.0.0.1", 8000, "source", "At3tQH9K4pKoDt", "/stream");
-        streamer.open();
-        cout << "Opened connection to icecast" << endl;
 
-        thread t([&](Streamer* streamer, const char* filename) {
-            streamer->send_mp3(filename);
-        }, &streamer, argv[1]);
-        t.detach();
+        cout << "Connecting to redis" << endl;
+        auto redis = Redis(redis_uri);
 
-        streamer.close();
-        cout << "Closed connection" << endl;
+        // Create a Subscriber.
+        auto sub = redis.subscriber();
+
+        // Set callback functions.
+        sub.on_message(
+            [&streamer](string channel, string msg)
+            {
+                if (channel == "channel1")
+                {
+                    cout << streamer.isPlaying() << endl;
+                }
+            });
+
+        // Subscribe to channels and patterns.
+        sub.subscribe(channel_name);
+
+        // Consume messages in a loop.
+        while (true)
+        {
+            try
+            {
+                sub.consume();
+            }
+            catch (const Error &err)
+            {
+                // Handle exceptions.
+                cout << err.what() << endl;
+                break;
+            }
+        }
+
+        // streamer.open();
+        // cout << "Opened connection to icecast" << endl;
+
+        // thread t([&](Streamer* streamer, const char* filename) {
+        //     streamer->send_mp3(filename);
+        // }, &streamer, argv[1]);
+        // t.detach();
+
+        // streamer.close();
+        // cout << "Closed connection" << endl;
     }
     catch (const runtime_error &e)
     {
---
+++ src/streamer.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <exception>
+#include <thread>
+
+using namespace std;
+
+extern "C" {
+    #include <shout/shout.h>
+}
+
+class Streamer {
+    private:
+        shout_t* instance;
+        thread* worker;
+
+        string hostname;
+        unsigned short port;
+        string user;
+        string password;
+        string mount;
+        size_t buffer_size = 4096;
+
+        vector<string> file_queue;
+        void send_mp3(string filename);
+        
+    public:
+        Streamer(
+            string hostname,
+            unsigned short port,
+            string user,
+            string password,
+            string mount
+        );
+
+        ~Streamer();
+
+        void open();
+        void close();
+        bool isPlaying();
+};
